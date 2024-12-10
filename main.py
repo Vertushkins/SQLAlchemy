b35ca07b3b71f317089e83a9034a54ae8c8fa8c7
Георгий Солодянкин <solodyankin.gm@edu.spbstu.ru>
2024-12-10 14:08:33 +0300

    Start

---
+++ main.py
@@ -0,0 +1,28 @@
+from sqlalchemy import Table, Column, Integer, String, MetaData, create_engine, text, insert
+
+
+def create_table(engine):
+    metadata.drop_all(engine)
+    metadata.create_all(engine)
+
+
+def insert_data():
+    with engine.connect() as connection:
+        statement = insert(user_table).values([
+            {"name": "Ivan"},
+            {"name": "Gosh"}
+        ])
+        connection.execute(statement)
+        connection.commit()
+
+
+engine = create_engine("sqlite:///data.db")
+metadata = MetaData()
+
+user_table = Table(
+    "workers", metadata,
+    Column("id", Integer, primary_key=True),
+    Column("name", String)
+)
+
+insert_data()
