fb2e74f78b6f23ec0a97fcbefa99a65d395a6b7f
Георгий Солодянкин <solodyankin.gm@edu.spbstu.ru>
2024-12-11 16:05:23 +0300

    resumes

--- models.py
+++ models.py
@@ -1,5 +1,8 @@
+import datetime
+
 from database import Base
 from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import ForeignKey, text
 
 
 class WorkersORM(Base):
@@ -7,4 +10,16 @@ class WorkersORM(Base):
 
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str]
+
+
+class ResumesORM(Base):
+    __tablename__ = "resumes"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    title: Mapped[str]
+    compensation: Mapped[int | None]
+    worker_id: Mapped[int] = mapped_column(ForeignKey("workers.id"))
+    created_at: Mapped[datetime.datetime] = mapped_column(server_default=text("TIMEZONE ('utc', now())"))
+    updated_at: Mapped[datetime.datetime] = mapped_column(server_default=text("TIMEZONE ('utc', now())"),
+                                                          onupdate=datetime.datetime.now())
 
